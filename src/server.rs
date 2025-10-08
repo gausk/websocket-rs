2d6db9ce31bd66656a5315dc370f1fb008be2268
Gaurav Kumar <gauravsingh12341@gmail.com>
2025-10-08 18:41:25 +0530

    echo websocket server

--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,8 @@
 use crate::SERVER_ADDRESS;
 use anyhow::Result;
-use futures_util::{SinkExt, StreamExt, TryStreamExt, future};
-use std::sync::Arc;
+use futures_util::{StreamExt, TryStreamExt, future};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::Mutex;
 use tokio_tungstenite::accept_async;
-use tokio_tungstenite::tungstenite::{Bytes, Message, Utf8Bytes};
 use tracing::{error, info};
 
 pub async fn run_websocket_server() -> Result<()> {
@@ -26,32 +23,9 @@ async fn handle_connection(stream: TcpStream) -> Result<()> {
     info!("Connection from peer addr: {}", addr);
     let ws_stream = accept_async(stream).await?;
     info!("Upgraded to WebSocket connection: {}", addr);
-    let (write, mut read) = ws_stream.split();
-    // let write = Arc::new(Mutex::new(write));
-    // while let Some(msg) = read.next().await {
-    //     let msg = msg?;
-    //     let out = match msg {
-    //         Message::Text(text) => {
-    //             Message::Text(text.as_str().chars().rev().collect::<String>().into())
-    //         }
-    //         Message::Binary(b) => Message::Binary(b.iter().copied().rev().collect()),
-    //         Message::Ping(_) => Message::Text(Utf8Bytes::from_static("Ping")),
-    //         Message::Pong(_) => Message::Text(Utf8Bytes::from_static("Pong")),
-    //         Message::Close(_) => Message::Binary(Bytes::from_static(b"closed")),
-    //         Message::Frame(_) => unreachable!(),
-    //     };
-    //     let mut write = write.lock().await;
-    //     write.send(out).await?;
-    // }
-    //// message coming delayed with this approach. Was working before reverse
+    let (write, read) = ws_stream.split();
+    // message coming delayed once try return after reverse
     read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
-        .map_ok(|msg| match msg {
-            Message::Text(text) => {
-                Message::Text(text.as_str().chars().rev().collect::<String>().into())
-            }
-            Message::Binary(b) => Message::Binary(b.iter().copied().rev().collect()),
-            _ => unreachable!(),
-        })
         .forward(write)
         .await?;
     Ok(())
